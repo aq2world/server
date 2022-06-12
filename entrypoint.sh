a3a0082bf2c857c671e5da19968307a71990fec7
Dino <8dino2@gmail.com>
2022-06-12 13:27:07 -0400

    Fixed SERVERTARGETDIR sed delimiter

--- entrypoint.sh
+++ entrypoint.sh
@@ -264,7 +264,7 @@ echo "map $default_map" >> /aq2server/action/config.cfg
 # Setup S3 vars to allow for demo uploading
 sed -i "s/AWS_ACCESS_KEY/$AWS_ACCESS_KEY/g" /home/admin/.s3cfg
 sed -i "s/AWS_SECRET_KEY/$AWS_SECRET_KEY/g" /home/admin/.s3cfg
-sed -i "s/SERVERTARGETDIR/$SERVERTARGETDIR/g" /aq2server/plugins/mvd_transfer.sh
+sed -i "s-SERVERTARGETDIR-$SERVERTARGETDIR-g" /aq2server/plugins/mvd_transfer.sh
 
 # Start the server!
 /aq2server/q2proded +set game action +set net_port $PORT +exec config.cfg +set q2a_config q2admin.lua
