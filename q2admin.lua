3edb9fd3c1b21a8a1090f33b724b324f10b33e13
Dino <8dino2@gmail.com>
2022-06-12 15:17:18 -0400

    Added q2admin plugin info

--- q2admin.lua
+++ q2admin.lua
@@ -5,5 +5,11 @@
 --
 
 plugins = {
+  mvd = {
+    mvd_webby = 'https://demos.aq2world.com',
+    exec_script_on_system_after_recording = '/aq2server/plugins/mvd_transfer.sh',
+    exec_script_cvars_as_parameters = {"q2a_mvd_file", "s3cmd"},
+    needs_cvar_q2a_mvd_autorecord = false
+  },
   version = {} -- version feedback script
 }
