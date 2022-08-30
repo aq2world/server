a54438c4c62ef6b01c5f214fdaef74ab302d8900
Dino <8dino2@gmail.com>
2022-08-30 10:33:04 -0400

    Made test build image modular

--- q2admin.lua
+++ q2admin.lua
@@ -1,17 +1,19 @@
 --
 -- Q2Admin example configuration
---
+-- 
 -- rename to "config.lua" and place this to quake 2 root
 --
 
 plugins = {
   mvd = {
-    mvd_webby = 'https://demos.aq2world.com',
-    exec_script_on_system_after_recording = '/aq2server/plugins/mvd_transfer.sh',
-    exec_script_cvars_as_parameters = {"q2a_mvd_file", "s3cmd"},
-    needs_cvar_q2a_mvd_autorecord = false
+          mvd_webby = 'https://demos.aq2world.com',
+          exec_script_on_system_after_recording = '/aq2server/plugins/mvd_transfer.sh',
+          exec_script_cvars_as_parameters = {"q2a_mvd_file", "s3cmd"},
+          needs_cvar_q2a_mvd_autorecord = false
   },
-  version = {} -- version feedback script
-  mvd2cprints = {}, -- add mising ACC,IMP,EXC centerprints information into mvd2 demos
-  coinflip = {}, -- toss a coin INGAME ;)
+  coinflip = {}, -- Heads & Tails script
+  broadcast = {}, -- Broadcast script
+  version = {}, -- version feedback script
+  mvd2cprints = {}, -- store centerprints from AQ2 into MVD2
+  scoreboardStore = {}, -- store scoreboard at match end into MVD2
 }
