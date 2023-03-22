29f3c68a5448d3cf0eeba3d647b0ef9297e4c6b1
adean <8dino2@gmail.com>
2023-03-22 15:05:43 -0400

    Forgot to add them to entrypoint...

--- entrypoint.sh
+++ entrypoint.sh
@@ -261,6 +261,11 @@ echo "set ltk_skill $LTK_SKILL" >> /aq2server/action/config.cfg
 echo "set ltk_chat $LTK_CHAT" >> /aq2server/action/config.cfg
 echo "set ltk_jumpy $LTK_JUMPY" >> /aq2server/action/config.cfg
 echo "set ltk_routing $LTK_ROUTING" >> /aq2server/action/config.cfg
+echo "set ltk_classic $LTK_BOTFILE" >> /aq2server/action/config.cfg
+echo "set attract_mode $ATTRACT_MODE" >> /aq2server/action/config.cfg
+echo "set attract_mode_botcount $ATTRACT_MODE_BOTCOUNT" >> /aq2server/action/config.cfg
+echo "set attract_mode_team $ATTRACT_MODE_TEAM" >> /aq2server/action/config.cfg
+echo "set attract_mode_newnames $ATTRACT_MODE_NEWNAMES" >> /aq2server/action/config.cfg
 
 # Limits
 echo "set fraglimit $FRAGLIMIT" >> /aq2server/action/config.cfg
