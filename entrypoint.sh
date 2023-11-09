165f1a4713ecc1f53871a02e1c1db2a8528aeb04
Aaron Dean <8dino2@gmail.com>
2023-11-09 14:51:35 -0500

    Set ENV ESP_RESPAWN_UVTIME

--- entrypoint.sh
+++ entrypoint.sh
@@ -364,8 +364,10 @@ echo "set esp_punish $ESP_PUNISH" >> /aq2server/action/config.cfg
 echo "set esp_leaderequip $ESP_LEADEREQUIP" >> /aq2server/action/config.cfg
 echo "set esp_leaderenhance $ESP_LEADERENHANCE" >> /aq2server/action/config.cfg
 echo "set esp_matchmode $ESP_MATCHMODE" >> /aq2server/action/config.cfg
+echo "set esp_respawn_uvtime $ESP_RESPAWN_UVTIME" >> /aq2server/action/config.cfg
 echo "set medkit_max $MEDKIT_MAX" >> /aq2server/action/config.cfg
 echo "set medkit_value $MEDKIT_VALUE" >> /aq2server/action/config.cfg
+
 
 ## Set logfile_prefix if stat_logs is enabled (official servers only)
 if [ ${STAT_LOGS} == "1" ]; then
